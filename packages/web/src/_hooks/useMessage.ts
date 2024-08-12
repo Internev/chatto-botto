cf409cbe1d414533afccec9c81eeb3235b935fbf
Internev <nev@internev.com>
2024-08-12 16:25:50 +1000

    useMessage also handles additional api calls

--- packages/web/src/_hooks/useMessage.ts
+++ packages/web/src/_hooks/useMessage.ts
@@ -1,9 +1,11 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useAppContext } from '@/_context/AppContext'
 import { IMessage } from '@/_context/types'
+import { continueClaudeConversation } from '@/_actions/claude'
+import speak from '@/_actions/speak'
 
 export const useMessage = () => {
-  const { dispatch } = useAppContext()
+  const { state, dispatch } = useAppContext()
 
   type AddMessageParams = Partial<Omit<IMessage, 'id'>>
   type UpdateMessageParams = { id: string } & Partial<Omit<IMessage, 'id'>>
@@ -37,6 +39,7 @@ export const useMessage = () => {
         agent,
       }
 
+      console.log('useMessage fired with action:', actionType, message)
       dispatch({ type: actionType, message })
     }
   }
@@ -47,6 +50,42 @@ export const useMessage = () => {
   const updateMessage = useCallback(createMessageHandler('UPDATE_MESSAGE'), [
     dispatch,
   ])
+
+  useEffect(() => {
+    const continueConversation = async () => {
+      const conversation = state.conversation
+      const lastMessage =
+        conversation &&
+        conversation.messages[conversation.messages.length - 1 || 0]
+      if (lastMessage?.agent === 'user') {
+        console.log('🧐 User message detected. Getting new Claude response...')
+        const languages = await continueClaudeConversation(conversation)
+        addMessage({
+          languages,
+          userId: 'botto',
+          originalLanguage: 'ja',
+          agent: 'bot',
+        })
+      } else if (lastMessage?.agent === 'bot' && !lastMessage.audioUrls?.main) {
+        console.log('🤖 Bot message detected. Generating audio...')
+        const activeText = lastMessage.languages?.['main']?.join(' ')
+        if (!activeText) {
+          throw new Error('No main language found in last message')
+        }
+        const speechBase64 = await speak(activeText)
+        const audioUrl = 'data:audio/mp3;base64,' + speechBase64
+        console.log('🔊 Audio URL:', audioUrl)
+        updateMessage({
+          ...lastMessage,
+          audioUrls: {
+            ...lastMessage.audioUrls,
+            main: audioUrl,
+          },
+        })
+      }
+    }
+    continueConversation()
+  }, [state.conversation])
 
   return { addMessage, updateMessage }
 }
