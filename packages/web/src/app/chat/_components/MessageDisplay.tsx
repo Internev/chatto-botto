da26feec2e7b8c596f01fc60d5c12746c7c79de8
Internev <nev@internev.com>
2024-08-12 16:26:46 +1000

    MessageDisplay does as its name suggests now
    Also includes audio now

--- packages/web/src/app/chat/_components/MessageDisplay.tsx
+++ packages/web/src/app/chat/_components/MessageDisplay.tsx
@@ -1,46 +1,11 @@
-import React, { useEffect } from 'react'
-
+import React from 'react'
 import { useAppContext } from '@/_context/AppContext'
-import { useMessage } from '@/_hooks/useMessage'
-import { continueClaudeConversation } from '@/_actions/claude'
 
 const MessageDisplay: React.FC = () => {
   const { state } = useAppContext()
-  const { addMessage, updateMessage } = useMessage()
-
-  useEffect(() => {
-    const continueConversation = async () => {
-      const conversation = state.conversation
-      const lastMessage =
-        conversation &&
-        conversation.messages[conversation.messages.length - 1 || 0]
-      if (lastMessage?.agent === 'user') {
-        console.log('User message detected. Getting new Claude response...')
-        const languages = await continueClaudeConversation(conversation)
-        addMessage({
-          languages,
-          userId: 'botto',
-          originalLanguage: 'ja',
-          agent: 'bot',
-        })
-      } else if (lastMessage?.agent === 'bot' && !lastMessage.audioUrls?.main) {
-        console.log('Bot message detected. Generating audio...')
-        // const activeText = lastMessage.languages['main'].join(' ')
-        // const speechBase64 = await window.electronAPI.textToSpeech(activeText)
-        // const audioUrl = 'data:audio/mp3;base64,' + speechBase64
-        // updateMessage({
-        //   ...lastMessage,
-        //   audioUrls: {
-        //     ...lastMessage.audioUrls,
-        //     main: audioUrl,
-        //   },
-        // })
-      }
-    }
-    continueConversation()
-  }, [state.conversation])
 
   const { messages } = state.conversation
+  console.log('MessageDisplay re-rendered at', new Date().toISOString())
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
       <div className="p-4 space-y-4 container mx-auto max-w-prose">
@@ -52,6 +17,12 @@ const MessageDisplay: React.FC = () => {
             } max-w-[70%]`}
           >
             {message.languages.main?.join(' ')}
+            {message.audioUrls?.main && (
+              <audio controls autoPlay>
+                <source src={message.audioUrls.main} type="audio/mpeg" />
+                Your browser does not support the audio element.
+              </audio>
+            )}
           </div>
         ))}
       </div>
