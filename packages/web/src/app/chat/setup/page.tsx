660d95d4b252afdf81b706f369b079656aae4065
Internev <nev@internev.com>
2024-08-12 16:26:54 +1000

    Chat setup stub

---
+++ packages/web/src/app/chat/setup/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+// app/chat/page.tsx
+import React, { useEffect } from 'react'
+
+// I am: gender
+// I want to speak with: gender
+// Difficulty
+// Scenario
+
+export default function SetupPage() {
+  return (
+    <div className="flex flex-col h-screen content-center">
+      Setup
+      <div className="flex-grow overflow-auto"></div>
+      <div className="h-16 bg-gray-100"></div>
+    </div>
+  )
+}
