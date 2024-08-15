b5d4d6eb3252470bdab3a49aefbebbd7412c52f9
Internev <nev@internev.com>
2024-08-15 12:58:53 +1000

    Add link to start new chat

--- packages/web/src/app/chat/page.tsx
+++ packages/web/src/app/chat/page.tsx
@@ -3,10 +3,14 @@
 import React, { useEffect } from 'react'
 import MessageDisplay from './_components/MessageDisplay'
 import ChatInput from './_components/ChatInput'
+import Link from 'next/link'
 
 export default function ChatPage() {
   return (
     <div className="flex flex-col h-screen content-center">
+      <Link className="top-0 ml-auto mr-4" href="/chat/setup">
+        Start a new chat
+      </Link>
       <div className="flex-grow overflow-auto">
         <MessageDisplay />
       </div>
