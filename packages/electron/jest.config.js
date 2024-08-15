dd6e4b2586ee962d863150d378936a6d925227e2
Internev <nev@internev.com>
2024-08-16 08:32:44 +1000

    Remove monorepo including electron
    Just nextjs for now

--- packages/electron/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], ????
-}
