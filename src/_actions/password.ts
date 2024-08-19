4a67237809dc75a5a7fcef2af5467774d90ac7bc
Internev <nev@internev.com>
2024-08-19 19:46:03 +1000

    Use correct environment variable for temp pass

--- src/_actions/password.ts
+++ src/_actions/password.ts
@@ -2,6 +2,6 @@
 
 export const validatePassword = async (password: string) => {
   if (!password || password.length === 0) return false
-  const validPassword = process.env['ALPHA_PASSWORD']
+  const validPassword = process.env['ALPHA_PW']
   return password === validPassword
 }
