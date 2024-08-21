d0127df2fb3799e1cb6f0e5a0e1d9b08fa422ced
Internev <nev@internev.com>
2024-08-21 16:04:00 +1000

    No need for password on local

--- src/_actions/password.ts
+++ src/_actions/password.ts
@@ -1,6 +1,7 @@
 'use server'
 
 export const validatePassword = async (password: string) => {
+  if (process.env.NODE_ENV === 'development') return true
   if (!password || password.length === 0) return false
   const validPassword = process.env['ALPHA_PW']
   return password === validPassword
