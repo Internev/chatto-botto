5778ad67a06be9cf82a9f04a8ce17b39ab6ff9fa
Internev <nev@internev.com>
2024-09-09 18:02:14 +1000

    Add signin route to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,11 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - signup
+     * - signup/signin (auth pages)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    '/((?!auth/signup|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    '/((?!auth/signup|auth/signin|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 }
