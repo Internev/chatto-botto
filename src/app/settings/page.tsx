08a5a3a347bb6ad20a37400950fb7d74be372b93
Internev <nev@internev.com>
2024-09-11 12:38:38 +1000

    Stub settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SettingsPage: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen p-4">
+      <div className="text-center">
+        <h1>Settings Page</h1>
+        <p>I haven't made this yet!</p>
+        <p className="text-4xl">🤷</p>
+      </div>
+    </div>
+  )
+}
+
+export default SettingsPage
