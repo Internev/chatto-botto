6d6f249bc288c9916ca3eef3d108f6bc3148191a
Internev <nev@internev.com>
2024-10-15 17:17:02 +1100

    Beta pw update

--- src/app/_lib/password.ts
+++ src/app/_lib/password.ts
@@ -4,5 +4,6 @@ export const validatePassword = async (password: string) => {
   if (process.env.NODE_ENV === 'development') return true
   if (!password || password.length === 0) return false
   const validPassword = process.env['BETA_PW']
-  return password === validPassword
+  const validPassword2 = process.env['BETA_PW2']
+  return password === validPassword || password === validPassword2
 }
